0760b62f0ce8dce8be08d2c0f54591177d80fedb
Helton Mori <heltonmori@gmail.com>
2020-04-12 18:55:27 -0300

    Header
    Desktop

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,15 +14,6 @@
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to Vuetify
         </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
       </v-col>
 
       <v-col
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,26 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          dark: {
+            primary: '#503683',
+            accent: '#FF4081',
+            secondary: '#63DED1',
+            success: '#4CAF50',
+            info: '#2196F3',
+            warning: '#FB8C00',
+            error: '#FF5252'
+          },
+          light: {
+            primary: '#503683',
+            accent: '#FF4081',
+            secondary: '#63DED1',
+            success: '#4CAF50',
+            info: '#2196F3',
+            warning: '#FB8C00',
+            error: '#FF5252'
+          }
+        }
+      }
 });
