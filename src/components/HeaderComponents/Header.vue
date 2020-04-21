c4c0c498f79f9ec5be687c52a47075c5d81b2385
Helton Mori <heltonmori@gmail.com>
2020-04-21 15:25:19 -0300

    Menu base desktop

--- src/components/HeaderComponents/Header.vue
+++ src/components/HeaderComponents/Header.vue
@@ -27,19 +27,35 @@
                 <span class="header-menu ml-2">Menu</span>
               </v-btn>
             </template>
+            <v-container class="img-container">
+              <v-row no-gutters>
+                <v-col cols="6">
+                  <v-img 
+                    src="../../assets/banner-menu.jpg" 
+                    contain
+                    min-height="100%"
+                    >
 
-            <v-list 
-              class="menu-container"
-              flat             
-              >
-              <v-list-item
-                v-for="(item, i) in items"
-                :key="i"
-                
-              >
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
+                  </v-img>
+                </v-col>
+                <v-col cols="6" class="menu-container">
+                  <v-list 
+                    class="list-menu"                    
+                    flat             
+                    >
+                    <v-list-item
+                      v-for="(item, i) in items"
+                      :key="i"
+                      class="label-item"
+                    >
+                      <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-col>
+              </v-row>
+            </v-container>
+
+
           </v-menu>
       </v-col>
 
@@ -90,6 +106,12 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Muli:wght@400;500;600;700&display=swap');
+
+* {
+  font-family: 'Muli', sans-serif;
+}
+
 .header-primary > div{
   max-width: 1300px!important;
   margin: 0 auto;
@@ -108,44 +130,6 @@ export default {
 .header-buttons > div {
   float: right;
 
-}
-
-.header-menu {
-  text-transform: uppercase;
-  font-size: 14px;
-  vertical-align: middle;
-}
-
-.my-menu, .header-buttons {
-  height: 100%;
-}
-
-.my-menu {
-  padding-left: 0!important;
-}
-
-.my-menu i.mdi-menu, .my-menu span {
-  color:  #63DED1!important;
-
-}
-
-.my-menu i.mdi-menu {
-  font-size: 45px;
-  font-weight: 800;
-}
-
-.menu-container {
-  width: 500px;
-  border-radius: 0!important;
-  width: 100%;
-  height: 100%;
-}
-div.v-menu__content {
-  top: 0!important;
-  left: 0!important;
-  border-radius: 0!important;
-  height: 100vh;
-  width: 70vw;
 }
 
 .header-buttons > div a {
@@ -176,6 +160,71 @@ div.v-menu__content {
 
 .header-buttons > div a.entre-em-contato:hover{
   color: #30ada0;
+}
+
+/* Menu - conferir se as classes estão todas aqui antes de separar em componente */
+
+.header-menu {
+  text-transform: uppercase;
+  font-size: 14px;
+  vertical-align: middle;
+}
+
+.my-menu, .header-buttons {
+  height: 100%;
+}
+
+.my-menu {
+  padding-left: 0!important;
+}
+
+.my-menu i.mdi-menu, .my-menu span {
+  color:  #63DED1!important;
+
+}
+
+.my-menu i.mdi-menu {
+  font-size: 45px;
+  font-weight: 800;
+}
+
+.menu-container {
+  background: #fff;
+  display: flex;
+  margin-top: 10%;
+  justify-content: center;
+}
+
+.list-menu {
+
+}
+
+.img-container {
+  padding: 0!important;
+}
+
+div.v-menu__content {
+  top: 0!important;
+  left: 0!important;
+  border-radius: 0!important;
+  height: 100vh;
+  width: 80vw;
+  overflow: hidden;
+  background-color: #fff;
+}
+
+div.v-menu__content > .container{
+  height: 100%;
+}
+
+.label-item {
+  margin: 25px 0;
+}
+
+.label-item > div {
+  font-size: 28px!important;
+  font-weight: 700;
+  color: #503683;
 }
 
 </style>
