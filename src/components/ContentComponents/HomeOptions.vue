7b06918db5737ab6b5bca9ad414e3e34deed3e50
Helton Mori <heltonmori@gmail.com>
2020-04-21 23:00:50 -0300

    Estrutura Menu Home

---
+++ src/components/ContentComponents/HomeOptions.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-container>
+            <v-row>
+                <v-spacer></v-spacer>
+                <v-col cols="2">
+                    <v-img src="../../assets/adote.png">insta</v-img>
+                </v-col>
+
+                <v-col cols="2">
+                    <v-img src="../../assets/temporario.png">pg estática</v-img>
+                </v-col>
+
+                <v-col cols="2">
+                    <v-img src="../../assets/doacao.png">pag estática/popup</v-img>
+                </v-col>
+
+                <v-col cols="2">
+                    <v-img src="../../assets/parceiro.png">pg estática</v-img>
+                </v-col>
+                <v-col cols="2">
+                    <v-img src="../../assets/voluntario.png">pg estática</v-img>
+                </v-col>
+                <v-spacer></v-spacer>
+            </v-row>
+        </v-container>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HomeOptions',
+  }
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <PageMessage />
+    <HomeOptions />
     <HelloWorld msg="Welcome"/>
   </div>
 </template>
@@ -10,12 +11,14 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import PageMessage from '@/components/ContentComponents/PageMessage.vue'
+import HomeOptions from '@/components/ContentComponents/HomeOptions.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
-    PageMessage
+    PageMessage,
+    HomeOptions
   }
 }
 </script>
