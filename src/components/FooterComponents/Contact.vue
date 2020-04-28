7e48d765f7d2253fce038b59b989d4d85443c80a
Helton Mori <heltonmori@gmail.com>
2020-04-27 22:53:28 -0300

    Footer
    Contato preparação para instalação de mais ícones

--- src/components/FooterComponents/Contact.vue
+++ src/components/FooterComponents/Contact.vue
@@ -1,25 +1,29 @@
 <template>
-  <v-container class="container-contato" primary>
-    <v-row>
-        <v-spacer></v-spacer>
-        <v-col cols="6" class="paragraph-text-container">
-            <h2 class="subtitle">Entre em contato!</h2>
-        </v-col>
-        <v-col cols="6">
-            <h3>Feirinha de Adoção</h3>
-            <span>
-                Praça do Bem - Em frente ao ATI do Ingá<br>
-                Entre a Av. XV de Novembro e São Paulo <br>
-                Domingo das 9h às 17h            
-            </span>
-            <ul>
-                <li>Instagram</li>
-                <li>Face</li>
-                <li>Whats</li>
-            </ul>
-        </v-col>
-        <v-spacer></v-spacer>
-    </v-row>
+  <v-container primary class="no-max-width">
+        <v-container class="container-contato" >
+            <v-row>
+                <v-spacer></v-spacer>
+                <v-col cols="6" class="paragraph-text-container subtitle-content-footer">
+                    <h2 class="subtitle">Entre em contato :)</h2>
+                </v-col>
+                <v-col cols="6">
+                    <div class="mini-container">
+                        <h3>Feirinha de Adoção</h3>
+                        <span>
+                            Praça do Bem - Em frente ao ATI do Ingá<br>
+                            Entre a Av. XV de Novembro e São Paulo <br>
+                            Domingo das 9h às 17h            
+                        </span>
+                        <ul>
+                            <li>Instagram</li>
+                            <li>Face</li>
+                            <li>Whats</li>
+                        </ul>
+                    </div>
+                </v-col>
+                <v-spacer></v-spacer>
+            </v-row>
+        </v-container>
   </v-container>
 </template>
 
@@ -35,7 +39,32 @@
     display: flex;
     justify-content: center;
     flex-direction: column;
-    min-height: 260px;
-    padding-top: 150px;
+    padding: 70px 50px;
+    color: #fff;
+}
+
+.container-contato h2 {
+    text-align: center;
+    font-weight: bolder;
+    font-size: 30px;
+    color: #fff;
+}
+
+.container-contato h3 {
+    margin-bottom: 20px;
+}
+
+.container-contato .mini-container {
+    display: table;
+    margin: auto
+}
+
+.container-contato .mini-container span {
+    color: #ccc;
+    font-weight: 300;
+}
+.subtitle-content-footer {
+    align-items: center!important;
+    justify-content: flex-start;
 }
 </style>
--- src/components/FooterComponents/Footer.vue
+++ src/components/FooterComponents/Footer.vue
@@ -1,8 +1,5 @@
 <template>
-    <v-container>
-        <Contact />
-        
-    </v-container>
+    <Contact />
 </template>
 
 <script>
