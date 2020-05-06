d71ec6856ef88fad5062f6de2e685fb15176d234
Helton Mori <heltonmori@gmail.com>
2020-05-05 23:50:44 -0300

    Iniciando Lar Temporario

---
+++ src/views/LarTemporario.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="container-message">
+    <v-row class="text-center" no-gutters>
+      <v-col cols="12" class="message">
+        <h1 class="first-text">
+            Dê um
+            <span class="second-text">
+                Lar temporário
+            </span>
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'LarTemporario',
+  }
+</script>
+
+<style>
+/**/
+</style>
