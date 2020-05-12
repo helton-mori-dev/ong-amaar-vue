d9a6b935436efd5162070827b8d3b1ae5ade97d1
Helton Mori <heltonmori@gmail.com>
2020-05-11 23:36:40 -0300

    Textos iniciais lar temporário

---
+++ src/components/ContentComponents/TextDark.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="container-message-dark">
+    <v-row>
+        <v-spacer></v-spacer>
+        <v-col cols="6" class="central-text">
+            A ONG Amaar não tem abrigo. Precisa da ajuda da comunidade para abrigar os pets resgatados. Graças a esta contribuição, conseguimos resgatar e fazer a doação de muitos animais. Venha fazer parte desta família e colabora com o lar temporário. Você só precisa ter um espacinho em casa e muito amor e compreensão com estes bichinhos.
+        </v-col>
+        <v-spacer></v-spacer>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'TextDark',
+  }
+</script>
+
+<style scoped>
+.container-message-dark {
+    background: #503683;
+    color: #ccc;
+    max-width: none;
+}
+</style>
--- src/views/LarTemporario.vue
+++ src/views/LarTemporario.vue
@@ -1,25 +1,46 @@
 <template>
-  <v-container class="container-message">
-    <v-row class="text-center" no-gutters>
-      <v-col cols="12" class="message">
-        <h1 class="first-text">
-          {{ firstMessage }}            
-            <span class="second-text">
-              {{ secondMessage }}                
-            </span>
-        </h1>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <v-container class="container-message">
+      <v-row class="text-center" no-gutters>
+        <v-col cols="12" class="message">
+          <h1 class="first-text">
+            {{ firstMessage }}            
+              <span class="second-text">
+                {{ secondMessage }}                
+              </span>
+          </h1>
+        </v-col>
+      </v-row>
+    </v-container>
+    <TextDark />
+    <v-container class="container-message" style="padding-top: 40px">
+      <v-row class="text-center" no-gutters>
+        <v-col cols="12" class="message">
+          <h1 class="first-text" style="line-height: 0">
+            {{ interrogacao }}            
+              <span class="second-text" style="font-size: 36px">
+                {{ comoFunciona }}                
+              </span>
+          </h1>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+  import TextDark from '@/components/ContentComponents/TextDark.vue'
   export default {
     name: 'LarTemporario',
+    components: {
+      TextDark
+    },
     data() {
       return {
         firstMessage: 'Dê um',
-        secondMessage: 'Lar temporário'
+        secondMessage: 'Lar temporário',
+        interrogacao: '?',
+        comoFunciona: 'Como funciona'
       }
     }
   }
