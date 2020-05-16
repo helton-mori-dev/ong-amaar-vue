3bba016b46640056ef441fdd4f1828dbd046b3ba
Helton Mori <heltonmori@gmail.com>
2020-05-16 01:07:09 -0300

    Página base parceiro

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/LarTemporario.vue')
+  },
+  {
+    path: '/sejaumparceiro',
+    name: 'SejaUmParceiro',
+    component: () => import('../views/SejaUmParceiro.vue')
   }
 ]
 
---
+++ src/views/SejaUmParceiro.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-container class="container-message">
+      <v-row class="text-center" no-gutters>
+        <v-col cols="12" class="message">
+          <h1 class="first-text">
+            {{ firstMessage }}            
+              <span class="second-text">
+                {{ secondMessage }}                
+              </span>
+          </h1>
+        </v-col>
+      </v-row>
+    </v-container>
+    
+    <v-container class="container-message-dark">
+      <v-row>
+          <v-spacer></v-spacer>
+          <v-col cols="6" class="central-text">
+              Você quer ser um parceiro? Através das parcerias, a ONG conseguiu pagar muitas das suas dívidas. <br>
+              Sua ajuda é muito importante!
+          </v-col>
+          <v-spacer></v-spacer>
+      </v-row>
+    </v-container>
+
+    <v-container class="container-options" primary>
+      <v-row class="text-center">
+          <v-col cols="2"></v-col>
+          <v-col cols="2"></v-col>
+          <v-col cols="2"></v-col>
+          <v-col cols="2"></v-col>
+          <v-col cols="2"></v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SejaUmParceiro',
+    data() {
+      return {
+        firstMessage: 'Seja um',
+        secondMessage: 'Parceiro'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.container-message-dark {
+    background: #503683;
+    color: #ccc;
+    max-width: none;
+}
+
+.container-message-dark .central-text {
+  font-size: 20px;
+}
+</style>
