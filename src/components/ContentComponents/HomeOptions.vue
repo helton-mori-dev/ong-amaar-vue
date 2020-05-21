60ddd57db3db5c241eb946ad298a004457f1d82c
Helton Mori <heltonmori@gmail.com>
2020-05-20 23:05:35 -0300

    Menu options responsivo

--- src/components/ContentComponents/HomeOptions.vue
+++ src/components/ContentComponents/HomeOptions.vue
@@ -1,32 +1,56 @@
 <template>
     <v-container class="container-options" primary>
-        <v-row class="text-center">
+        <v-row class="text-center d-none d-sm-none d-md-flex">
             <v-col cols="2" class="adote">
                 <v-img class="img-adote" height="320px" src="../../assets/adote.png"></v-img>
                 <v-img class="adote-hover d-none" height="320px" src="../../assets/adote-hover.png"></v-img>
-                <a class="options-link" target="blank" href="https://www.instagram.com/ongamaar/">Adote um<span>Animalzinho</span></a>
+                <a class="options-link" target="blank" href="https://www.instagram.com/ongamaar/">Adote um<span> Animalzinho</span></a>
             </v-col>
             <v-col cols="2" class="temporario">
                 <v-img class="img-temporario" height="320px" src="../../assets/temporario.png"></v-img>
                 <v-img class="temporario-hover d-none" height="320px" src="../../assets/temporario-hover.png"></v-img>
-                <a class="options-link" href="/lartemporario">Dê um<span>Lar temporário</span></a>
+                <a class="options-link" href="/lartemporario">Dê um<span> Lar temporário</span></a>
             </v-col>
 
             <v-col cols="2" class="doacao">
                 <v-img class="img-doacao" height="320px" src="../../assets/doacao.png"></v-img>
                 <v-img class="doacao-hover d-none" height="320px" src="../../assets/doacao-hover.png"></v-img>
-                <a class="options-link" href="#">Faça uma<span>Doação</span></a>
+                <a class="options-link" href="#">Faça uma<span> Doação</span></a>
             </v-col>
 
             <v-col cols="2" class="parceiro">
                 <v-img class="img-parceiro" height="320px" src="../../assets/parceiro.png"></v-img>
                 <v-img class="parceiro-hover d-none" height="320px" src="../../assets/parceiro-hover.png"></v-img>
-                <a class="options-link" href="sejaumparceiro">Seja um<span>parceiro</span></a>
+                <a class="options-link" href="sejaumparceiro">Seja um<span> parceiro</span></a>
             </v-col>
             <v-col cols="2" class="voluntario">
                 <v-img class="img-voluntario" height="320px" src="../../assets/voluntario.png"></v-img>
                 <v-img class="voluntario-hover d-none" height="320px" src="../../assets/voluntario-hover.png"></v-img>
-                <a class="options-link" href="sejaumvoluntario">Seja um<span>Voluntário</span></a>
+                <a class="options-link" href="sejaumvoluntario">Seja um<span> Voluntário</span></a>
+            </v-col>
+        </v-row>
+        <v-row class="d-flex d-md-none">
+            <v-col cols="12" class="adote">
+                <v-img class="img-adote" src="../../assets/adote-mobile.png"></v-img>
+                <a class="options-link" target="blank" href="https://www.instagram.com/ongamaar/">Adote um<span> Animalzinho</span></a>
+            </v-col>
+            <v-col cols="12" class="temporario">
+                <v-img class="img-temporario" src="../../assets/temporario-mobile.png"></v-img>
+                <a class="options-link" href="/lartemporario">Dê um<span> Lar temporário</span></a>
+            </v-col>
+
+            <v-col cols="12" class="doacao">
+                <v-img class="img-doacao" src="../../assets/doacao-mobile.png"></v-img>
+                <a class="options-link" href="#">Faça uma<span> Doação</span></a>
+            </v-col>
+
+            <v-col cols="12" class="parceiro">
+                <v-img class="img-parceiro" src="../../assets/parceiro-mobile.png"></v-img>
+                <a class="options-link" href="sejaumparceiro">Seja um<span> parceiro</span></a>
+            </v-col>
+            <v-col cols="12" class="voluntario">
+                <v-img class="img-voluntario" src="../../assets/voluntario-mobile.png"></v-img>
+                <a class="options-link" href="sejaumvoluntario">Seja um<span> Voluntário</span></a>
             </v-col>
         </v-row>
         <v-row class="text-center anchor-button">
@@ -74,7 +98,6 @@
 }
 
 .container-options .options-link span{
-    display: block;
     font-weight: 900;
 }
 
@@ -82,38 +105,53 @@
     padding: 80px 0;
 }
 
-.container-options .adote:hover .img-adote{
-    display: none;
-}
-.container-options .adote:hover .adote-hover{
-    display: flex!important;
+
+@media screen and (max-width:960px) {
+    .container-options .options-link {
+        width: 100%;
+        text-align: center;
+    }
 }
 
-.container-options .temporario:hover .img-temporario{
-    display: none;
-}
-.container-options .temporario:hover .temporario-hover{
-    display: flex!important;
-}
+@media screen and (min-width:960px){
+    .container-options .options-link span{
+        display: block;
+        font-weight: 900;
+    }
 
-.container-options .doacao:hover .img-doacao{
-    display: none;
-}
-.container-options .doacao:hover .doacao-hover{
-    display: flex!important;
-}
+    .container-options .adote:hover .img-adote{
+        display: none;
+    }
+    .container-options .adote:hover .adote-hover{
+        display: flex!important;
+    }
 
-.container-options .parceiro:hover .img-parceiro{
-    display: none;
-}
-.container-options .parceiro:hover .parceiro-hover{
-    display: flex!important;
-}
+    .container-options .temporario:hover .img-temporario{
+        display: none;
+    }
+    .container-options .temporario:hover .temporario-hover{
+        display: flex!important;
+    }
 
-.container-options .voluntario:hover .img-voluntario{
-    display: none;
-}
-.container-options .voluntario:hover .voluntario-hover{
-    display: flex!important;
+    .container-options .doacao:hover .img-doacao{
+        display: none;
+    }
+    .container-options .doacao:hover .doacao-hover{
+        display: flex!important;
+    }
+
+    .container-options .parceiro:hover .img-parceiro{
+        display: none;
+    }
+    .container-options .parceiro:hover .parceiro-hover{
+        display: flex!important;
+    }
+
+    .container-options .voluntario:hover .img-voluntario{
+        display: none;
+    }
+    .container-options .voluntario:hover .voluntario-hover{
+        display: flex!important;
+    }    
 }
 </style>
