52bbb47b6bb41f7140b7322c64e77848b90743e3
Helton Mori <heltonmori@gmail.com>
2020-05-20 21:47:24 -0300

    Scroll nas imagens antes-depois

--- src/components/ContentComponents/BeforeAfter.vue
+++ src/components/ContentComponents/BeforeAfter.vue
@@ -2,12 +2,13 @@
   <v-container class="container-antes-depois no-max-width" lightbackground>
     
     <v-row class="margin-auto flex-center">
-        <v-col cols="8" class="central-text-antes-depois">
+        <v-col cols="12" sm="8" class="central-text-antes-depois">
             <h2>Antes / Depois</h2>
             <span>A ONG Amaar resgatou muitos animais em situação de rua. Confira a foto de antes e depois de alguns dos bichinhos que passaram por aqui!</span>
         </v-col>
     </v-row>
-    <v-row class="text-center">
+    <v-col class="horizontal-scroll" cols="12">
+      <v-row class="text-center linha-scroll">
         <v-col cols="3">
             <v-img src="../../assets/antesdepois1.jpg"></v-img>
         </v-col>
@@ -22,7 +23,8 @@
         <v-col cols="3">
             <v-img src="../../assets/antesdepois4.jpg"></v-img>
         </v-col>
-    </v-row>
+      </v-row>
+    </v-col>
   </v-container>
 </template>
 
@@ -43,7 +45,7 @@
   justify-content: center;
   align-items: center;
 }
-.container-antes-depois > .row {
+.container-antes-depois .linha-scroll {
     max-width: 1300px;
     margin: auto;
     justify-content: center;
@@ -56,7 +58,6 @@
   font-size: 24px;
   padding-bottom: 20px;
 }
-
 .container-antes-depois .central-text-antes-depois span{
   font-size: 17px;
   font-weight: 400;
@@ -66,5 +67,20 @@
     text-align: center;
     color: #604298;
     margin-bottom: 10px;
+}
+@media screen and (max-width: 960px) {
+
+  .horizontal-scroll {
+    overflow: scroll;    
+  }
+
+  .container-antes-depois .linha-scroll {
+      max-width: none;
+      width: 1350px;
+  }
+
+  .container-antes-depois > .linha-scroll > div {
+    max-width: 24%;
+  }
 }
 </style>
