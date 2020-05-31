b1a2846b4eb94086949b430802ec5ea103f2c148
Helton Mori <heltonmori@gmail.com>
2020-05-30 23:32:11 -0300

    Ancora doação

--- src/views/FacaUmaDoacao.vue
+++ src/views/FacaUmaDoacao.vue
@@ -24,7 +24,32 @@
           
       </v-row>
     </v-container>
+      <v-row class="text-center anchor-button-doacao">
+          <v-col>
+              <a href="#ancora-doacao">
+                  <v-icon large color="secondary">
+                      mdi-arrow-down
+                  </v-icon>
+              </a>
+                  
+          </v-col>
+      </v-row>
+
     
+  <v-container class="container-message-doacao" id="ancora-doacao">
+    <v-row class="text-center" no-gutters>
+      <v-col cols="12" class="message">
+          <p class="first-text-conheca">
+              ?
+              <span class="second-text-conheca">
+                  Qual o impacto das suas doações
+              </span>
+          </p>
+      </v-col>
+    </v-row>
+
+  </v-container>
+
   </div>
 </template>
 
@@ -53,5 +78,19 @@
 .container-banner .row-banner {
   text-align: center;
   justify-content: center;  
+}
+.anchor-button-doacao {
+    padding: 40px 0;
+    background: #503683;
+}
+
+.container-message-doacao {
+    background:  #fff;
+    max-width: 1300px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    min-height: 260px;
+    padding-top: 150px;
 }
 </style>
