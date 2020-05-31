11d1b11e6d687e88eb57822d30a9091a7ae3a82b
Helton Mori <heltonmori@gmail.com>
2020-05-31 00:24:36 -0300

    Primeira mensagem doação

--- src/views/FacaUmaDoacao.vue
+++ src/views/FacaUmaDoacao.vue
@@ -37,7 +37,7 @@
 
     
   <v-container class="container-message-doacao" id="ancora-doacao">
-    <v-row class="text-center" no-gutters>
+    <v-row class="text-center mb-10 container-historia" no-gutters>
       <v-col cols="12" class="message">
           <p class="first-text-conheca">
               ?
@@ -47,7 +47,18 @@
           </p>
       </v-col>
     </v-row>
-
+    <v-row class="mt-10">
+      <v-spacer></v-spacer>
+      <v-col cols="12" sm="4">
+          <v-img src="../assets/doacao/banner-doacao.png"></v-img>
+      </v-col>
+      <v-col cols="12" sm="8" class="paragraph-text-container">
+          <h2 class="subtitle">Mais de 960 animais conseguiram ser adotados!</h2>
+          <p class="paragraph-text">Esta é a felícia, ela tem apenas 6 meses e conseguiu ser adotada. Ela vai à feira de adoção desde os 45 dias. graças às doações da comunidade conseguimos resgatar muitos animais em situação de rua e mantê-los protegidos em lares temporários. Agradecemos à comunidade por todo apoio, sem vocês não conseguiríamos realizar este trabalho tão incrível!</p>
+        
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
   </v-container>
 
   </div>
@@ -86,11 +97,12 @@
 
 .container-message-doacao {
     background:  #fff;
-    max-width: 1300px;
+    max-width: 1100px;
     display: flex;
     justify-content: center;
     flex-direction: column;
     min-height: 260px;
-    padding-top: 150px;
+    padding-top: 100px;
 }
+
 </style>
