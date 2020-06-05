b7b12d765a8918e396446eeaa29694d13e89f3be
Helton Mori <heltonmori@gmail.com>
2020-06-04 22:46:15 -0300

    Teste para corrigir as rotas

--- src/router/index.js
+++ src/router/index.js
@@ -4,41 +4,37 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/lartemporario',
-    name: 'LarTemporario',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/LarTemporario.vue')
-  },
-  {
-    path: '/sejaumparceiro',
-    name: 'SejaUmParceiro',
-    component: () => import('../views/SejaUmParceiro.vue')
-  },
-  {
-    path: '/sejaumvoluntario',
-    name: 'SejaUmVoluntario',
-    component: () => import('../views/SejaUmVoluntario.vue')
-  },
-  {
-    path:'/facaumadoacao',
-    name: 'FacaUmaDoacao',
-    component: () => import('../views/FacaUmaDoacao.vue')
-  }
-]
-
-const router = new VueRouter({
+export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/lartemporario',
+      name: 'lartemporario',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/LarTemporario.vue')
+    },
+    {
+      path: '/sejaumparceiro',
+      name: 'sejaumparceiro',
+      component: () => import('../views/SejaUmParceiro.vue')
+    },
+    {
+      path: '/sejaumvoluntario',
+      name: 'sejaumvoluntario',
+      component: () => import('../views/SejaUmVoluntario.vue')
+    },
+    {
+      path:'/facaumadoacao',
+      name: 'facaumadoacao',
+      component: () => import('../views/FacaUmaDoacao.vue')
+    }
+  ]
 })
-
-export default router
