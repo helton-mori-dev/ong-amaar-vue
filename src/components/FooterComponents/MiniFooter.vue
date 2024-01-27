617d12699b1d2b8982649529a7a662df6f56b090
Helton Mori <heltonmori@gmail.com>
2024-01-27 16:40:13 -0300

    Ajuste ano footer e Publicpath

--- src/components/FooterComponents/MiniFooter.vue
+++ src/components/FooterComponents/MiniFooter.vue
@@ -1,25 +1,29 @@
 <template>
-    <v-footer height="60" class="mini-footer">
-        <label for="">2020 - ONG Amaar</label>        
-    </v-footer>
+  <v-footer height="60" class="mini-footer">
+    <label for="">{{ ano }} - ONG Amaar</label>
+  </v-footer>
 </template>
 
 <script>
-
 export default {
-    name: 'MiniFooter'
-}    
+  name: "MiniFooter",
+  data() {
+    return {
+      ano: new Date().getFullYear(),
+    };
+  },
+};
 </script>
 
 <style>
 .mini-footer {
-    width: 100%;
+  width: 100%;
 }
 
-.mini-footer label{
-    text-align: center;
-    width: 100%;
-    font-weight: 800;
-    color: #503683;
+.mini-footer label {
+  text-align: center;
+  width: 100%;
+  font-weight: 800;
+  color: #503683;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/ong-amaar-vue/" : "/",
+  publicPath: process.env.NODE_ENV === "/ong-amaar-vue/",
 };
